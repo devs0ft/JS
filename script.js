1960adfc2d46d35a53783290fbc6d767000fdec8
devs0ft <devsoft78@gmail.com>
2022-03-10 18:04:41 +0000

    Day 3

---
+++ script.js
@@ -0,0 +1,22 @@
+let firstName = "John";
+let lastName = "Doe";
+console.log(firstName);
+console.log(lastName);
+
+let fruits = ["apple", "guava", "orange", "mango"];
+console.log(fruits);
+console.log(fruits.length);
+let person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 12,
+};
+console.log(person);
+console.log(fruits.length);
+console.log(fruits[0]);
+console.log(fruits.push("banana"));
+console.log(fruits.pop());
+console.log(fruits.shift());
+console.log(fruits.unshift("pineapple"));
+console.log(fruits.includes("beans"));
+console.log(fruits.join("_"));
